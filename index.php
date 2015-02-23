30065b57feef98e8ada293e9ce4f1b8bdefab1ef
Oleg <oarch@mail.ru>
2015-02-23 20:46:37 +0300

    контроллер форм - бд

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/models/table.php';
 
 $table = new Table('articles');
-$articles = $table->getAll();   // getAll($news)
+$articles = $table->getAll('news');   // getAll($news)
 
 include_once __DIR__.'/views/list.php';
 
