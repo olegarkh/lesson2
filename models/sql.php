8588b8a55eb3e20fe32279219f0abe3f070ed8ef
Oleg <oarch@mail.ru>
2015-02-23 18:39:08 +0300

    Функции для работы с бд

--- models/sql.php
+++ models/sql.php
@@ -26,8 +26,6 @@ function Disconnection(){
 
 function sql_Query($query){
 
-    //sql_Connect();
-
     $row = mysql_query( $query );
 
     $dba = [];
@@ -37,10 +35,8 @@ function sql_Query($query){
     return $dba;
 }
 function get_record($query){
-    //sql_Connect();
 
     $row = mysql_query($query);
-    //return mysql_fetch_array($row);
 
     if ($record = mysql_fetch_array($row)){
         return $record;
@@ -58,16 +54,14 @@ function toDB($id, $dir, $filename, $comment, $visited){
 }
 
 function record_toDB($article){
-    //Connection('news');
-
     $query = "INSERT INTO articles ( pub_date, title, preview, text )
                         VALUES('$article->date', '$article->title', '$article->preview ', '$article->text')";
     if ($res = mysql_query($query)){
-        //echo '$article->id = ' . title ."<br>";
         return true;
     }
     return false;
 }
+
 function update_record($article){
     $query = "UPDATE articles SET pub_date = '$article->date',
                                      title = '$article->title',
@@ -77,7 +71,6 @@ function update_record($article){
     if ($res = mysql_query($query)){
         return true;
     }
-    //echo 'Функция update_record не работает ! ' ;
     return false;
 }
 
