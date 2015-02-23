35ad4c0d4a3a6e4f950ae05608cedca0af3ea22d
Oleg <oarch@mail.ru>
2015-02-23 16:16:09 +0300

    Вывод списка новостей

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
 
-require_once __DIR__.'/models/article.php';
 require_once __DIR__.'/models/table.php';
 
-//$article = new TArticle('');
-//$article->Create();
 $table = new Table('articles');
-//var_dump($table);
-//$news = new News($table);
-//$news->ShowAll();
-$articles = $table->getAll(); // getAll($news)
-
-//$article = new TArticle($items);
+$articles = $table->getAll();   // getAll($news)
 
 include_once __DIR__.'/views/list.php';
-//$article->showList();
+
